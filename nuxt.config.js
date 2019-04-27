3a56f54d2ff3eb0e7de9ad6a8b83258e55087fbf
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-04-27 22:09:57 +0200

    Vuetify added

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,22 +3,18 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "todos",
+    title: 'todos',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  /*
-   ** Global CSS
-   */
-  css: ["tachyons/css/tachyons.css"],
   /*
    ** Customize the progress bar color
    */
-  loading: { color: "#3B8070" },
+  loading: { color: '#3B8070' },
   /*
    ** Build configuration
    */
@@ -29,9 +25,9 @@ module.exports = {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
         });
       }
